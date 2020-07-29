96b07f8eeb3f8308662414abd98a656a30d99247
jayyu <jayyu@123u.com>
2020-07-29 19:30:20 +0800

    ss

--- bin/code4fgui.js
+++ bin/code4fgui.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 const program = require('commander');
 const Main = require('./bundle/Main')
 
 program
-    .version('1.0.1', '-v, --version')
+    .version('1.0.11', '-v, --version')
     .command('gencode <project> [output]')
     .description('生成fgui的代码，参数1为fgui工程根目录, 参数2位代码 输出目录')
     .action((project, output)=>{
