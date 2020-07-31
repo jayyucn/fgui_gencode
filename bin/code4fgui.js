1f320fbb4e502703b199411dadc0459e48cebbb6
jayyu <jayyu@123u.com>
2020-07-31 09:38:37 +0800

    ss

--- bin/code4fgui.js
+++ bin/code4fgui.js
@@ -3,15 +3,18 @@
 const program = require('commander');
 const { default: Main } = require('./bundle/Main');
 require('./libs/StringExtends');
-
+if (process.argv.length <= 2) {
+    console.log(`[usage] 示例：
+    code4fgui - p path-of-client -o path-of-output -t path-of-template`)
+    process.exit(0);
+}
 program
-    .version('1.0.0', '-v, --version')
+    .version('1.0.30', '-v, --version')
     .usage("示例：code4fgui -p path-of-client -o path-of-output -t path-of-template")
     .option('-p --project-path <in>', "[必填] 客户端工程根目录")
     .option('-o --output-path <out>', "[必填] 代码输出目录")
     .option('-t --template-Path <tp>', "[选填] 代码模板目录")
     .parse(process.argv);
-
 if (!program.projectPath)
     throw new Error("缺少参数：-p [client path]客户端地址,可填相对地址");
 if (!program.outputPath)
